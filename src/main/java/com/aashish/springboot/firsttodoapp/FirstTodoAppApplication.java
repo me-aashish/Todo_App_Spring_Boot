b4f4d484e6612677b2448859e67a34bcfaa619ab
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-02-01 14:15:46 +0530

    basic set up done

---
+++ src/main/java/com/aashish/springboot/firsttodoapp/FirstTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.aashish.springboot.firsttodoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aashish/springboot/firsttodoapp/FirstTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashish.springboot.firsttodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
